8a8478a2741f8f508920f4854614d5a91753a558
StephenAraka <stevenaraka@gmail.com>
2022-06-14 21:32:24 +0300

    add db config

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": process.env.DATABASE_HOST,
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
